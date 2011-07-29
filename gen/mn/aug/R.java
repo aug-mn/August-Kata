7b98e1694eb6395abdb8929cdf34cbf20a7296b1
justingrammens <justin@localtone.com>
2011-07-28 22:27:03 -0500

    make call to get google icon with slow network traffic

--- gen/mn/aug/R.java
+++ gen/mn/aug/R.java
@@ -15,7 +15,7 @@ public final class R {
     }
     public static final class id {
         public static final int btn_load_webview=0x7f050000;
-        public static final int wv_page=0x7f050001;
+        public static final int main_imageview=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/mn/aug/KataAsynchronicityActivity.java
+++ src/mn/aug/KataAsynchronicityActivity.java
@@ -1,36 +1,51 @@
 package mn.aug;
 
+import java.io.InputStream;
+import java.net.URL;
+
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.webkit.WebView;
 import android.widget.Button;
-import mn.aug.R;
+import android.widget.ImageView;
 
 public class KataAsynchronicityActivity extends Activity {
-	
-	private WebView webView;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    
-    Button btn = (Button) findViewById(R.id.btn_load_webview);
-    webView = (WebView) findViewById(R.id.wv_page);
-    btn.setOnClickListener(new OnClickListener() {
-		
-		public void onClick(View v) {
-			loadWebView("http://www.google.com");
-		}
-	});
-}
 
+	ImageView imageView;
+	private String TAG = this.getClass().getName();
 
-    private void loadWebView(String url) {
-    	webView.loadUrl(url);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button btn = (Button) findViewById(R.id.btn_load_webview);
+		imageView = (ImageView) findViewById(R.id.main_imageview);
+		btn.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				Drawable d = getImage("http://www.google.com/intl/en_com/images/srpr/logo2w.png");
+				imageView.setImageDrawable(d);
+			}
+		});
 	}
 
+	private Drawable getImage(String url) {
+		try {
+			// simulate slow network traffic..
+			Thread.sleep(2000);
+			
+			InputStream is = (InputStream) new URL(url).getContent();
+			Drawable d = Drawable.createFromStream(is, "src name");
+			return d;
+
+		} catch (Exception ex) {
+			Log.d(TAG, "Exception getting image: " + ex.getMessage());
+			return null;
+		}
+	}
 
 }
