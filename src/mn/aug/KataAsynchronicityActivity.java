58bd048f3be33381b01adc25fcfd651aa9f4c942
Ian Dees <ian.dees@gmail.com>
2011-08-02 19:56:09 -0500

    Move the work intensive stuff into an AsyncTask.

--- src/mn/aug/KataAsynchronicityActivity.java
+++ src/mn/aug/KataAsynchronicityActivity.java
@@ -4,11 +4,14 @@ import java.io.InputStream;
 import java.net.URL;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -20,16 +23,42 @@ public class KataAsynchronicityActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		setProgressBarIndeterminateVisibility(false);
+		
 		setContentView(R.layout.main);
+		
 
 		Button btn = (Button) findViewById(R.id.btn_load_webview);
 		imageView = (ImageView) findViewById(R.id.main_imageview);
 		btn.setOnClickListener(new OnClickListener() {
 
-			public void onClick(View v) {
-				Drawable d = getImage("http://www.google.com/intl/en_com/images/srpr/logo2w.png");
-				imageView.setImageDrawable(d);
-			}
+            public void onClick(View v) {
+                new AsyncTask<String, Void, Drawable>() {
+                    
+                    @Override
+                    protected void onPreExecute() {
+                        setProgressBarIndeterminateVisibility(true);
+                    }
+
+                    @Override
+                    protected void onProgressUpdate(Void... values) {
+                    }
+
+                    @Override
+                    protected Drawable doInBackground(String... params) {
+                        Drawable d = getImage(params[0]);
+                        return d;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Drawable result) {
+                        setProgressBarIndeterminateVisibility(false);
+                        imageView.setImageDrawable(result);
+                    }
+                    
+                }.execute("http://www.google.com/intl/en_com/images/srpr/logo2w.png");
+            }
 		});
 	}
 
