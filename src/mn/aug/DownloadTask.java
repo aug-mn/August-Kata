afb6d241cfb98b0d0babe28488582115a85e093c
Daniel Lew <danlew42@gmail.com>
2011-08-02 19:59:57 -0500

    Split DownloadTask into own class

---
+++ src/mn/aug/DownloadTask.java
@@ -0,0 +1,64 @@
+package mn.aug;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+class DownloadTask extends AsyncTask<String, Void, Drawable> {
+
+	String TAG = this.getClass().getName();
+
+	/**
+	 * 
+	 */
+	private ImageView imageView;
+	private ProgressBar progressBar;
+
+	/**
+	 * @param kataAsynchronicityActivity
+	 */
+	DownloadTask(ImageView imageView, ProgressBar progressBar) {
+		this.imageView = imageView;
+		this.progressBar = progressBar;
+	}
+
+	@Override
+	protected void onPreExecute() {
+		progressBar.setVisibility(View.VISIBLE);
+	}
+
+	@Override
+	protected Drawable doInBackground(String... params) {
+		return getImage(params[0]);
+	}
+
+	@Override
+	protected void onPostExecute(Drawable result) {
+		if (result != null) {
+			imageView.setImageDrawable(result);
+		}
+		progressBar.setVisibility(View.GONE);
+	}
+
+	Drawable getImage(String url) {
+		try {
+			// simulate slow network traffic..
+			Thread.sleep(2000);
+
+			InputStream is = (InputStream) new URL(url).getContent();
+			Drawable d = Drawable.createFromStream(is, "src name");
+			return d;
+
+		}
+		catch (Exception ex) {
+			Log.d(TAG, "Exception getting image: " + ex.getMessage());
+			return null;
+		}
+	}
+}
--- src/mn/aug/KataAsynchronicityActivity.java
+++ src/mn/aug/KataAsynchronicityActivity.java
@@ -1,13 +1,7 @@
 package mn.aug;
 
-import java.io.InputStream;
-import java.net.URL;
-
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -17,9 +11,8 @@ import android.widget.ProgressBar;
 public class KataAsynchronicityActivity extends Activity {
 
 	ImageView imageView;
-	private String TAG = this.getClass().getName();
 	
-	private ProgressBar mProgressBar;
+	ProgressBar mProgressBar;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -33,45 +26,10 @@ public class KataAsynchronicityActivity extends Activity {
 		btn.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
-				(new DownloadTask()).execute("http://www.google.com/intl/en_com/images/srpr/logo2w.png");
+				(new DownloadTask(imageView, mProgressBar)).execute("http://www.google.com/intl/en_com/images/srpr/logo2w.png");
 			}
 		});
 	}
 
-	private Drawable getImage(String url) {
-		try {
-			// simulate slow network traffic..
-			Thread.sleep(2000);
-
-			InputStream is = (InputStream) new URL(url).getContent();
-			Drawable d = Drawable.createFromStream(is, "src name");
-			return d;
-
-		}
-		catch (Exception ex) {
-			Log.d(TAG, "Exception getting image: " + ex.getMessage());
-			return null;
-		}
-	}
-
-	private class DownloadTask extends AsyncTask<String, Void, Drawable> {
-
-		@Override
-		protected void onPreExecute() {
-			mProgressBar.setVisibility(View.VISIBLE);
-		}
-
-		@Override
-		protected Drawable doInBackground(String... params) {
-			return getImage(params[0]);
-		}
-
-		@Override
-		protected void onPostExecute(Drawable result) {
-			if (result != null) {
-				imageView.setImageDrawable(result);
-			}
-			mProgressBar.setVisibility(View.GONE);
-		}
-	}
+	
 }
